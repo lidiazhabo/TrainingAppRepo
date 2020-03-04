40914b08ddf5b1e40028aba20e98bb05d6bc6515
Lidia <lidiia.zhabo@globallogic.com>
2020-03-04 13:38:03 +0200

    Initial commit

---
+++ TrainingApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TrainingApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
